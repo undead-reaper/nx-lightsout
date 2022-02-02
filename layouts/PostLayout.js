3aea24652c6d49e78387be825e0bca3bf9630c62
Dharam Soni <dharamsoni1010@gmail.com>
2022-02-03 02:38:03 +0530

    Updated footer and post layout

--- layouts/PostLayout.js
+++ layouts/PostLayout.js
@@ -68,14 +68,14 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
                       <dl className="whitespace-nowrap text-sm font-medium leading-5">
                         <dt className="sr-only">Name</dt>
                         <dd className="text-gray-900 dark:text-gray-100">{author.name}</dd>
-                        <dt className="sr-only">Twitter</dt>
+                        <dt className="sr-only">Instagram</dt>
                         <dd>
-                          {author.twitter && (
+                          {author.instagram && (
                             <Link
-                              href={author.twitter}
+                              href={author.instagram}
                               className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
                             >
-                              {author.twitter.replace('https://twitter.com/', '@')}
+                              {author.instagram.replace('https://www.instagram.com/', '@')}
                             </Link>
                           )}
                         </dd>
